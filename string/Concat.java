114059cec1346b8a26de6b3f8887ea9c1f13fbc2
skdhg <46562212+skdhg@users.noreply.github.com>
2023-07-21 10:22:25 +0545

    s

---
+++ string/Concat.java
@@ -0,0 +1,16 @@
+package string;
+
+public class Concat {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "Script";
+        int n = 4;
+        String c = "Ever";
+
+        // method 1
+        System.out.println(a + b + n + c);
+
+        // method 2
+        System.out.println(a.concat(b).concat(n + c));
+    }
+}
