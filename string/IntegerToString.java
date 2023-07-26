cb2ccc5d9fc887fc385d8eeb215bf8ee3cdbeefc
skdhg <46562212+skdhg@users.noreply.github.com>
2023-07-26 10:34:10 +0545

    int to str

---
+++ string/IntegerToString.java
@@ -0,0 +1,17 @@
+package string;
+
+public class IntegerToString {
+    public static void main(String[] args) {
+        int a = 10, b = -10;
+
+        String str1 = Integer.toString(a);
+        String str2 = b + "";
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        Double d = 10.5;
+
+        System.out.println(d.toString());
+    }
+}
